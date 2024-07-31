3dc0261f33be38b1651f697ab4cd81b170bdadfa
dev@penguin <deivh@yahoo.fr>
2024-07-31 09:00:57 +0200

    settings file created the first time

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
 import configparser
 import tkinter as tk
+import os 
+  
+
 
 # OnTop = 0
 # Wrap = 1
@@ -17,7 +20,15 @@ class MemPadSettings:
     def __init__(self):
         conf = configparser.ConfigParser(default_section = "Main", allow_no_value=True ) 
         conf.optionxform = lambda option: option
-        conf.read(MemPadSettings.app_path + "/settings.ini") 
+
+        self.settings_file = MemPadSettings.app_path + "/settings.ini"
+  
+        if not os.path.exists(self.settings_file): 
+            with open(self.settings_file, 'w') as file: 
+                file.write(self.get_default_settings()) 
+                file.close()
+
+        conf.read(self.settings_file ) 
 
         self.conf = conf
         self.values = {}
@@ -36,9 +47,38 @@ class MemPadSettings:
         return MemPadSettings._instance
  
 
+    def get_default_settings(self):
+        return """
+[Main]
+ext = .lst
+Iwidth = 296
+Font = Tahoma,16,0,$FFFFFF
+iFont = Tahoma,11,0,$FFFFFF
+BGcolor = $804000
+BGIcolor = $224488
+OnTop = 0
+Wrap = 1
+EditLock = 0
+ExitEsc = 1
+AutoSave = 0
+SearchRange = 101
+searchX = 0
+searchY = 0
+NoBackup = 1
+Win98crypt = 0
+DecryptFailWarn = 0
+MRU =  MemPadX.lst
+LatestFiles = MemPadX.lst
+NumKeep = 10
+winX = 0
+winY = 136
+WinWidth = 800
+WinHeight = 600
+"""
+
     def save(self):
 
-        with open(MemPadSettings.app_path + '/settings.ini', 'w') as configfile:
+        with open(self.settings_file, 'w') as configfile:
             self.conf.write(configfile)
 
     def getVariable(self, name, var_type = 'str'):
